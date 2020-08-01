5dc2394d5ef797a0f04d87b6062c542b5359df58
Pedro Augusto <paferreira982@hotmail.com>
2020-08-01 14:17:36 -0300

    att-tratar-nome

--- pramic/javascript/tratamento-input.js
+++ pramic/javascript/tratamento-input.js
@@ -1,5 +1,17 @@
-function tratarNome (id) {
-    let caracteresAceitos = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwYyVvXxZz-ÁáÉéÍíÓóÚúÃãÕõÂâÊêÎîÔôÛûàÀ";
-    let auxId = document.getElementById(id).value;
+function tratarNome(id) {
+    let caracteresAceitos = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwYyVvXxZzÁáÉéÍíÓóÚúÃãÕõÂâÊêÎîÔôÛûàÀ ";
+    let auxId = document.getElementById(id).value, aux = "";
+    aux = auxId.length;
+    for (let i = 0; i < aux; i++) {
+        if (caracteresAceitos.includes(auxId.charAt(i)) == false) {
+            auxId = auxId.replace(auxId.charAt(i), "");
+            i--;
+        }
+    }
+
+    while (auxId.includes("  ")) {
+        auxId = auxId.replace("  ", " ");
+    }
     
+    document.getElementById(id).value = auxId.trim();
 }
