0d491b72980aac7d86988e62123a37a54a0c9b80
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2020-08-01 07:52:12 -0300

    Disponibilizando ficha cadastral de voluntario para ser inserida na pagina

--- pramic/cadastro_voluntario.php
+++ pramic/cadastro_voluntario.php
@@ -1,1 +1,25 @@
-
+<fieldset>
+    <legend>Seja um voluntário</legend>
+    <form action = "">
+        <label>
+            <span>Nome: <br></span>
+            <input type = "text" name = "nome" placeholder = "Insira seu nome">
+        </label>
+        <label>
+            <span>Idade: <br></span>
+            <input type = "text" name = "idade" placeholder = "Insira sua idade"><br>
+        </label>
+        <label>
+            <span>Telefone (Opcional): <br></span>
+            <input type = "text" name = "telefone" placeholder = "Digite apenas números"><br>
+        </label>
+        <label>
+            <span>Email: <br></span>
+            <input type = "text" name = "email" placeholder = "exemplo@meuemail.com"><br>
+        </label>
+        <label>
+            <span>CPF: <br></span>
+            <input type = "text" name = "cpf" placeholder = "Digite apenas números"><br>
+        </label>
+    </form>
+</fieldset>
