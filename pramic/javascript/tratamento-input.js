beb9ad2539fd70e47e2fb48133d245c91da7598b
Pedro Augusto <paferreira982@hotmail.com>
2020-08-01 14:35:58 -0300

    att-tratamento-telefone
    att

--- pramic/javascript/tratamento-input.js
+++ pramic/javascript/tratamento-input.js
@@ -1,5 +1,5 @@
 function tratarNome(id) {
-    let caracteresAceitos = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwYyVvXxZzÁáÉéÍíÓóÚúÃãÕõÂâÊêÎîÔôÛûàÀ ";
+    let caracteresAceitos = "AaBbÇçCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwYyVvXxZzÁáÉéÍíÓóÚúÃãÕõÂâÊêÎîÔôÛûàÀ ";
     let auxId = document.getElementById(id).value, aux = "";
     aux = auxId.length;
     for (let i = 0; i < aux; i++) {
@@ -12,6 +12,21 @@ function tratarNome(id) {
     while (auxId.includes("  ")) {
         auxId = auxId.replace("  ", " ");
     }
-    
+
+    document.getElementById(id).value = auxId.trim();
+}
+
+function tratarTelefone(id) {
+    let caracteresAceitos = "0123456789";
+    let auxId = document.getElementById(id).value, aux = "";
+    aux = auxId.length;
+
+    for (let i = 0; i < aux; i++) {
+        if (caracteresAceitos.includes(auxId.charAt(i)) == false) {
+            auxId = auxId.replace(auxId.charAt(i), "");
+            i--;
+        }
+    }
+
     document.getElementById(id).value = auxId.trim();
 }
