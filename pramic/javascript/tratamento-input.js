d6da55556341640b61d38c11829e75559459c5d5
Pedro Augusto <paferreira982@hotmail.com>
2020-08-01 14:57:47 -0300

    att-tratamento-email

--- pramic/javascript/tratamento-input.js
+++ pramic/javascript/tratamento-input.js
@@ -30,3 +30,73 @@ function tratarTelefone(id) {
 
     document.getElementById(id).value = auxId.trim();
 }
+
+function verificarEmail(id) {
+    let email = document.getElementById(id).value;
+    let caracteresValidos = "@._-ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwyxz0123456789"
+    let contadorArroba = 0;
+    email.trim();
+
+    for (let i = 0; i < email.length; i++) {
+        if (email.charAt(i) == "@") {
+            contadorArroba++;
+        }
+    }
+
+    if (contadorArroba != 1) {
+        document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+        contadorArroba = 0;
+        return false;
+    }
+
+    if (email.charAt(email.length - 1) == "." || email.charAt(email.length - 1) == "-" || email.charAt(email.length - 1) == "_") {
+        document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+        contadorArroba = 0;
+        return false;
+    }
+
+    for (let i = 0; i < 10; i++) {
+        if (parseInt(email.charAt(0)) == i || email.charAt(0) == ".") {
+            document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+            contadorArroba = 0;
+            return false;
+        }
+    }
+
+    for (let i = 0; i < caracteresValidos.length; i++) {
+        if (caracteresValidos.indexOf(email.charAt(i)) == -1) {
+            document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+            contadorArroba = 0;
+            i = 1000;
+            return false;
+        }
+    }
+
+    for (let i = 0; i < email.length; i++) {
+        if (email.charAt(i) == "." && email.charAt(i + 1) == ".") {
+            document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+            contadorArroba = 0;
+            return false;
+        }
+    }
+
+    let localizacaoArroba = email.indexOf("@");
+    let corte = email.substr(localizacaoArroba, email.length);
+
+    if (corte.length < 9 || (corte.includes(".com") == false)) {
+        document.getElementById(id).style.backgroundColor = "rgba(255, 110, 110, 0.726)";
+        document.getElementById(id).style.border = "red";
+        contadorArroba = 0;
+        return false;
+    } else {
+        document.getElementById(id).style.backgroundColor = "white";
+        document.getElementById(id).style.border = "black";
+        contadorArroba = 0;
+        return true;
+    }
+}
