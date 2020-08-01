e472c66dc73bc15f55e5ac0a2f9cdbde9c189d2c
Arthur de Rosen <arthurderosen@hotmail.com>
2020-08-01 14:25:09 -0300

    'mudança de pastas e formulario inclusao e alteracao de eventos'

--- pramic/associacao/cadastro_voluntario.php
+++ pramic/associacao/cadastro_voluntario.php
@@ -1,92 +1,94 @@
 <fieldset>
     <legend>Cadastro de voluntário</legend>
-    <form action = "">
+    <form action="">
         <label>
             <span>Nome:<br></span>
-            <input type = "text" name = "nome" placeholder = "Insira seu nome"><br>
+            <input type="text" name="nome" placeholder="Insira seu nome"><br>
         </label>
         <label>
             <span>CPF:<br></span>
-            <input type = "text" name = "cpf" placeholder = "Digite apenas números"><br>
+            <input type="text" name="cpf" placeholder="Digite apenas números"><br>
         </label>
         <label>
             <span>Idade:<br></span>
-            <input type = "text" name = "idade" placeholder = "Insira sua idade"><br>
+            <input type="text" name="idade" placeholder="Insira sua idade"><br>
         </label>
         <label>
             <span>Telefone (Opcional):<br></span>
-            <input type = "text" name = "telefone" placeholder = "Digite apenas números"><br>
+            <input type="text" name="telefone" placeholder="Digite apenas números"><br>
         </label>
         <label>
             <span>Email:<br></span>
-            <input type = "text" name = "email" placeholder = "exemplo@meuemail.com"><br>
+            <input type="text" name="email" placeholder="exemplo@meuemail.com"><br>
         </label>
-        <input type = "submit" value = "Enviar">
+        <input type="submit" value="Enviar">
     </form>
 </fieldset>
 
 <fieldset>
     <legend>Cadastro de associação</legend>
-    <form action = "">
+    <form action="">
         <label>
             <span>Nome da associação:<br></span>
-            <input type = "text" name = "nome-associacao" placeholder = "Insira o nome da sua associação"><br>
+            <input type="text" name="nome-associacao" placeholder="Insira o nome da sua associação"><br>
         </label>
         <label>
             <span>Comunidade:<br></span>
-            <select name = "comunidade">
-                <option value = "acari">Acari</option>
-                <option value = "cidade-de-deus">Cidade de Deus</option>
-                <option value = "complexo-do-alemao">Complexo do Alemão</option>
-                <option value = "jacarezinho">Jacarezinho</option>
-                <option value = "mangueira">Mangueira</option>
-                <option value = "manguinhos">Manguinhos</option>
-                <option value = "mare">Maré</option>
-                <option value = "parada-de-lucas">Parada de Lucas</option>
-                <option value = "rocinha">Rocinha</option>
-                <option value = "santa-marta">Santa Marta</option>
+            <select name="comunidade">
+                <option value="acari">Acari</option>
+                <option value="cidade-de-deus">Cidade de Deus</option>
+                <option value="complexo-do-alemao">Complexo do Alemão</option>
+                <option value="jacarezinho">Jacarezinho</option>
+                <option value="mangueira">Mangueira</option>
+                <option value="manguinhos">Manguinhos</option>
+                <option value="mare">Maré</option>
+                <option value="parada-de-lucas">Parada de Lucas</option>
+                <option value="rocinha">Rocinha</option>
+                <option value="santa-marta">Santa Marta</option>
             </select><br>
         </label>
         <label>
             <span>Telefone 1:<br></span>
-            <input type = "text" name = "telefone-um" placeholder = "Digite apenas números"><br>
+            <input type="text" name="telefone-um" placeholder="Digite apenas números"><br>
         </label>
         <label>
             <span>Telefone 2:<br></span>
-            <input type = "text" name = "telefone-dois" placeholder = "Digite apenas números"><br>
+            <input type="text" name="telefone-dois" placeholder="Digite apenas números"><br>
         </label>
         <label>
             <span>Email:<br></span>
-            <input type = "text" name = "email" placeholder = "exemplo@meuemail.com"><br>
+            <input type="text" name="email" placeholder="exemplo@meuemail.com"><br>
         </label>
-        <input type = "submit" value = "Enviar">
+        <input type="submit" value="Enviar">
     </form>
 </fieldset>
 
 <fieldset>
     <legend>Cadastrar novo evento</legend>
-    <form action = "">
+    <form action="">
         <label>
             <span>Nome do evento:<br></span>
-            <input type = "text" name = "nome-evento" placeholder = "Insira o nome do evento"><br>
+            <input type="text" name="nome-evento" placeholder="Insira o nome do evento"><br>
         </label>
         <label>
             <span>Descrição das atividades<br></span>
-            <textarea></textarea><br> <!--Editar-->
+            <textarea></textarea><br>
+            <!--Editar-->
         </label>
         <label>
             <span>Local de encontro<br></span>
-            <textarea></textarea><br> <!--Editar-->
+            <textarea></textarea><br>
+            <!--Editar-->
         </label>
         <label>
             <span>Data e hora<br></span>
-            <input type = "date" name = "data"> &nbsp;
-            <input type = "time" name = "hora"><br>
+            <input type="date" name="data"> &nbsp;
+            <input type="time" name="hora"><br>
         </label>
         <label>
             <span>Imagem (Opcional):<br></span>
-            <input type = "file" name = "imagem-evento"><br>
+            <input type="file" name="imagem-evento"><br>
         </label>
     </form>
-    <input type = "submit" value = "Enviar">
+    <input type="submit" value="Enviar">
 </fieldset>
---
+++ pramic/associacao/eventos/alterar_evento.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="" type="text/css" />
+</head>
+
+<body>
+    <form method="post" enctype="multipart/form-data" autocomplete="off" action="_update_evento.php">
+        <label for="titulo">Título</label>
+        <input type="text" id="titulo" name="titulo" maxlength="100" size="150" /><br>
+
+        <label for="descricao">Descrição</label>
+        <textarea type="text" id="descricao" name="descricao" maxlength="800" rows="18"></textarea><br>
+
+        <input type="checkbox" id="limpeza" name="limpeza" />
+        <label for="limpeza">Limpeza</label> <br>
+
+        <input type="checkbox" id="encanamento" name="encanamento" />
+        <label for="encanamento">Reparos de encanamento</label> <br>
+
+        <input type="checkbox" id="dengue" name="dengue" />
+        <label for="dengue">Combate a Dengue e Chicungunha</label> <br>
+
+        <input type="checkbox" id="valao" name="valao" />
+        <label for="valao">Segurança e sinalização em torno de valões</label> <br>
+
+        <label>Imagem</label>
+        <input type="file" id="imagem" name="imagem" /><br>
+
+        <label>Local</label>
+        <input type="text" id="local" name="local" maxlength="150" size="150" /><br>
+
+        <label>Data</label>
+        <input type="date" id="data" name="data" value="" min="" max="" /><br>
+
+        <label>Hora</label>
+        <input type="number" id="hora" name="hora" min="00" max="24" /> :
+        <input type="number" id="minutos" name="minutos" min="00" max="60" /><br>
+    </form>
+</body>
+
+</html>
