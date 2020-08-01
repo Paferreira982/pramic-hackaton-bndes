a152968f044e1067d2ddc597227b4261be3f008f
Pedro Augusto <paferreira982@hotmail.com>
2020-08-01 13:47:40 -0300

    att-js-1

---
+++ pramic/javascript/tratamento-input.js
@@ -0,0 +1,5 @@
+function tratarNome (id) {
+    let caracteresAceitos = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwYyVvXxZz-ÁáÉéÍíÓóÚúÃãÕõÂâÊêÎîÔôÛûàÀ";
+    let auxId = document.getElementById(id).value;
+    
+}
