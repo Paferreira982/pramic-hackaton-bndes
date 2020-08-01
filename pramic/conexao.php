66120c5c22f63a279c15ecd63fc7da7d4a04cab1
Arthur de Rosen <arthurderosen@hotmail.com>
2020-07-31 23:26:14 -0300

    'novos arquivos php'

---
+++ pramic/conexao.php
@@ -0,0 +1,23 @@
+<?php
+$conexao = mysqli_connect("127.0.0.1", "root", "", "pramic");
+
+if (!$conexao) {
+    echo "Error: Não possível conectar ao banco de dados." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+/* $query = "SELECT nome FROM comunidades";
+$resultado = mysqli_query($conexao, $query);
+
+if (mysqli_num_rows($resultado) > 0) {
+    while ($linha = mysqli_fetch_assoc($resultado)) {
+        echo $linha['nome'] . "\n";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conexao);
+ */
