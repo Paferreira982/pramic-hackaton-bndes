92e77019f3eb3841558ece3d1e4bc98e3d2fc774
Arthur de Rosen <arthurderosen@hotmail.com>
2020-08-02 11:54:15 -0300

    mudança cadastro voluntarios

--- pramic/home.php
+++ pramic/home.php
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/home.css" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <img src="imagens/pramic-logo.png" alt="logo" id="logo" />
+        <nav>
+            <a href="home.php" class="text-nav">PÁGINA INICIAL &nbsp;&nbsp;|</a>
+            <a href="mural_eventos.php" class="text-nav">MURAL DE EVENTOS &nbsp;&nbsp;|</a>
+            <a href="cadastro_associacao.php" class="text-nav">CADASTRO DE ASSOCIAÇÕES</a>
+        </nav>
+    </header>
+
+    <main>
+        <div id="botoes-div">
+            <a href="mural_eventos.php">
+                <button id="botao-voluntario">Quero ser um voluntário</button>
+            </a>
+            <a href="associacao/login.php">
+                <button id="botao-associado">Sou uma associação</button>
+            </a>
+        </div>
+    </main>
+
+    <footer>
+        <div id="rodape">
+            <div class="social">
+                <img src="imagens/facebook-black-icon.png"><br>
+                <span>/PramicRJ</span>
+            </div>
+            <div class="social">
+                <img src="imagens/twitter-black-icon.png"><br>
+                <span>@Pramic</span>
+            </div>
+            <div class="social">
+                <img src="imagens/whatsapp-black-icon.png"><br>
+                <span>xxxxx-xxxx</span>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
